bd15cf2a225d8427c50e8ebb30ad4b8ad2e8d816
Joseph <jskufca@clarkson.edu>
2022-12-04 13:29:59 -0500

    WIP Realtime bikeshare logging

---
+++ f2022/R/readbike.R
@@ -0,0 +1,12 @@
+# Script file written to perform a single read of gbfs data.
+#
+# The goal is to have a source file that will read a pull from gbfs data and
+# then append to csv file.
+
+library(gbfs)
+library(dplyr)
+
+
+
+
+
