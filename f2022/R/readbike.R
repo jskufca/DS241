13207d2101950eb4411a8ac812db5526a020225c
Joe Skufca <jskufca@clarkson.edu>
2022-12-05 08:46:43 -0500

    Real time logging code

--- f2022/R/readbike.R
+++ f2022/R/readbike.R
@@ -6,7 +6,21 @@
 library(gbfs)
 library(dplyr)
 
+filename="E:/Dropbox/Rwork/DS241/f2022/data/likelog.csv"
 
+df1=get_station_status(city = "cabi",output = "return") %>%
+  filter(station_id %in%  c("101","102","103","104","105")) %>% 
+  select(station_id,is_renting,is_returning,
+         num_docks_available,num_docks_disabled,
+         num_bikes_available,num_bikes_disabled,
+         num_ebikes_available,last_updated)
+
+write.table(df1,file=filename,append=TRUE,
+            row.names = FALSE,
+            col.names=FALSE,
+            sep = ",")
+  
+  
 
 
 
